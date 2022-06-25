131faea6115631f80aedda66c1162888ee6be271
Prharsha G <63843351+gsvprharsha@users.noreply.github.com>
2022-06-25 20:38:18 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+PURPLE='\033[1;35m'
+NC='\033[0m'
+
+
+if [ "$EUID" -ne 0 ]
+  then echo " "
+  echo -e "${RED}[-] Please run as root${NC}"
+  echo " "
+  exit
+else
+
+echo " "
+echo -e "${GREEN}[+] Installing Dependencies for ${NC}${PURPLE}Br0ther_Ey3${NC}"
+echo " "
+pip3 install requests
+echo " "
+pip3 install termcolor
+echo " "
+cp broeye /usr/bin/broeye
+chmod +x /usr/bin/broeye
+echo -e "${PURPLE}[+] Br0ther_Ey3 Successfully Installed.....Type (sudo broeye or broeye) in your terminal to check...${NC}"
+
+fi
